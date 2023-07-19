16e09a03645176ea0b771121a341203940096518
Abi0l9 <cryptog3ms@gmail.com>
2023-07-19 10:58:37 +0100

    handled adding new blog to server from the client

--- frontend/src/components/Main/BlogCard.jsx
+++ frontend/src/components/Main/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlogCard = ({ image, title, date, category, author }) => {
   return (
-    <div className="w-full flex flex-row md:flex-col md:w-[22%] mb-3 md:mb-5 rounded-[5px]">
+    <div className="w-full flex flex-row md:flex-col md:w-[22%] mb-3 md:mb-8 rounded-[5px]">
       <div className="h-[200px] w-2/4 md:w-full bg-gray-200 rounded-[10px]">
         <img
           src={`${image}`}
@@ -11,13 +11,15 @@ const BlogCard = ({ image, title, date, category, author }) => {
         />
       </div>
       <div className="w-2/4 md:w-full md:h-[170px] md:flex md:flex-col md:justify-between pl-2 md:pl-0 md:py-2">
-        <div className="md:h-[120px]">
+        <div className="md:h-[100px]">
           <p className="text-2xl md:text-xl flex-grow">{title}</p>
         </div>
-        <div className="md:h-[50px]">
+        <div className="md:h-[30px]">
           <p className="text-base md:font-semibold text-gray-800 my-3">
             {author}
           </p>
+        </div>
+        <div className="md:h-[20px]">
           <div className="flex flex-row items-center">
             <div className="text-xs my-3 text-gray-700 mr-6">{date}</div>
           </div>
--- frontend/src/components/Publish/index.jsx
+++ frontend/src/components/Publish/index.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import { Icon } from "@iconify/react";
 import backupIcon from "@iconify/icons-material-symbols/backup";
 import back from "@iconify/icons-material-symbols/arrow-back-ios-new";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import BlogServices from "../../services/blog";
 
 const Publish = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [tags, setTags] = useState("");
+  const [featuredImg, setFeaturedImg] = useState("");
+
+  const handleChange = (e, fn) => {
+    const value = e.target.value;
+    fn(value);
+  };
+
+  const uploadImg = (e) => {
+    const target = e.target;
+    const file = target?.files[0];
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      const img = e.target?.result;
+      setFeaturedImg(img);
+    };
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { title, body, author, tags, featuredImg };
+    try {
+      await BlogServices.addBlog(data);
+
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <div className="p-3  md:px-8 min-h-screen lg:px-10">
       <div className="flex flex-row items-center mb-5 w-full cursor-pointer">
@@ -13,7 +53,7 @@ const Publish = () => {
         </Link>
         <div className="text-xl text-blue-500">New Blog Post</div>
       </div>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <div className="flex flex-col w-full md:w-3/4">
           <div className="">
             <label htmlFor="title" className="block font-semibold">
@@ -23,6 +63,8 @@ const Publish = () => {
               className="w-full p-1 mt-1 mb-2 border-[1px] border-gray-500 focus:border-blue-200 rounded-[5px]"
               id="title"
               name="title"
+              value={title || ""}
+              onChange={(e) => handleChange(e, setTitle)}
               placeholder="New title"
             />
           </div>
@@ -32,8 +74,10 @@ const Publish = () => {
             </label>
             <textarea
               className="w-full p-1 mt-1 mb-2 border-[1px] border-gray-500 focus:border-blue-200 rounded-[5px] resize-none"
-              id="content"
-              name="content"
+              id="body"
+              name="body"
+              value={body}
+              onChange={(e) => handleChange(e, setBody)}
               rows="10"
               placeholder="The content of the blog can be written here..."
             ></textarea>
@@ -46,6 +90,8 @@ const Publish = () => {
               className="w-full md:w-2/4 p-1 mt-1 mb-2 border-[1px] border-gray-500 focus:border-blue-200 rounded-[5px]"
               id="author"
               name="author"
+              value={author}
+              onChange={(e) => handleChange(e, setAuthor)}
               placeholder="e.g James"
             />
           </div>
@@ -58,6 +104,8 @@ const Publish = () => {
               className="w-full md:w-2/4 p-1 mt-1 mb-2 border-[1px] border-gray-500 focus:border-blue-200 rounded-[5px]"
               id="tags"
               name="tags"
+              value={tags}
+              onChange={(e) => handleChange(e, setTags)}
               placeholder="sport, music,..."
             />
             <div className="text-xs text-gray-500 -mt-2">
@@ -65,13 +113,21 @@ const Publish = () => {
             </div>
           </div>
           <div className="my-2">
-            <div
-              className="w-full my-2 md:w-2/4 border-dashed border-2 border-gray-400 h-[200px] bg-gray-200
-          rounded-[10px]"
-            ></div>
+            {featuredImg && (
+              <div
+                className="w-full my-2 md:w-2/4 border-dashed border-2 border-gray-400 h-[300px] bg-gray-200
+            rounded-[10px]"
+              >
+                <img
+                  src={`${featuredImg}`}
+                  alt="upload"
+                  className="w-full h-[100%] object-cover rounded-[8px]"
+                />
+              </div>
+            )}
             <label
               htmlFor="featured"
-              className="w-[300px] block font-semibold p-2 bg-blue-800 text-white active:bg-blue-500 rounded-[5px] flex flex -row items-center justify-around"
+              className="w-[300px] md:w-2/4 block font-semibold p-2 bg-blue-800 text-white active:bg-blue-500 rounded-[5px] flex flex -row items-center justify-around"
             >
               <Icon icon={backupIcon} width={24} />
               Upload featured image
@@ -81,6 +137,8 @@ const Publish = () => {
               id="featured"
               name="featured"
               type="file"
+              accept="image/*"
+              onChange={uploadImg}
             />
           </div>
           <div className="w-3/4 md:w-2/4 border-[2px] border-blue-800 rounded-[5px] my-4 mx-auto text-center active:bg-blue-800 active:text-white">
