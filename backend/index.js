364ba600dad3227eb517d5c4b93e4bc31158a57c
Abi0l9 <cryptog3ms@gmail.com>
2023-07-20 22:20:50 +0100

    port changed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 require("./db");
 
-app.get("", (request, response) => response.send("Welcome home"));
 app.use("/api/blogs", BlogRouter);
 app.use("/api/emails", EmailRouter);
 
